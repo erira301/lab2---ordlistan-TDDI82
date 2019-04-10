f25a65b6784e323b791261824965aa59ee359904
erira301 <47356581+erira301@users.noreply.github.com>
2019-04-10 12:33:57 +0200

    Add files via upload

--- Ordlistan.cc
+++ Ordlistan.cc
@@ -1,90 +1,83 @@
-
 #include "Ordlistan.h"
 
-worder::worder(std::ifstream & is):word_count{}
+void worder::out_data(std::ostream & os,char print_type,int cnt =0)
 {
-  
-  auto to_map = [this](std::string str)
+  int n{};
+  auto print= [&os] (std::pair<std::string,unsigned int> item)
     {
-      auto hej = this->word_count.emplace(str,1); 
-      if(!hej.second)
-	{
-	  ++(hej.first)->second;
-	}
-     
+      os<<item.first<<": "<<item.second<<std::endl;    
+    };
+  auto compare = [](std::pair<std::string,unsigned int> const& lhs, std::pair<std::string,unsigned int> const& rhs)
+    {
+      return lhs.second > rhs.second;
+    };
+  auto printlad = [&n,cnt,&os](std::string str)
+    {
+      n+=str.size();
+      if(n > cnt){
+	n=str.size()+1;
+	os<<'\n';
+      }
+      os<<str<<' ';
     };
   
-  std::vector<std::string> stinger;
-  stinger = readwords(is);
+  std::map<std::string,unsigned int> map_words;
 
-  std::for_each(stinger.begin(), stinger.end(), to_map);
-}
-
-
-void worder::out_data(std::ostream & os)
-{
-  auto print= [&os] (std::pair<std::string,unsigned int> item)
-  {
-    os<<item.first<<": "<<item.second<<std::endl;
-    
-  };
-
-    std::for_each(word_count.begin(),word_count.end(),print);
-  
+  std::for_each(words.begin(),words.end(),[&map_words](std::string s){map_words[s]++;});
+  std::vector<std::pair<std::string, unsigned int>> setofwords {map_words.begin(),map_words.end()};
+  switch (print_type)
+    {
+    case 'a':
+      std::for_each(setofwords.begin(), setofwords.end(), print);
+      break;
+    case 'f':
+      std::sort(setofwords.begin(),setofwords.end(),compare);
+      std::for_each(setofwords.begin(),setofwords.end(),print);
+      break;
+    case 'o':
+      std::for_each(words.begin(), words.end(), printlad);
+      break;
+    default:
+      throw std::logic_error{"Wrong second argument"};
+      break;
+    }
 }
 
 std::vector<std::string> worder::readwords(std::ifstream & is)
 {
-  std::string line;
-  std::vector<std::string> strings;
-  while(is >> line)
+  auto cleanse = [](std::string str)
     {
-      clean(line);
-      if(islegal(line))
+      std::transform(str.begin(), str.end(), str.begin(), ::tolower);
+      str.erase(0,str.find_first_not_of("\"\'()") );
+      if(!str.empty())
 	{
-	  strings.push_back(line);
-	  std::cout << line << std::endl;
+	  str.erase(str.find_last_not_of(" ?!;:,.\"\')")+1);
 	}
-    }
-  return strings;
-}
-  
-void worder::clean(std::string & str)
-{
-  std::list<char>const forbidden_start{'\"', '\'', '('};
-  std::list<char>const forbidden_end{'?', '!', ';', ':', ',',
-      '.', '\"', '\'', ')'};
-  while(forbidden_start.end() != std::find(forbidden_start.begin(), forbidden_start.end(), str.front()))
-    {
-      str.erase(str.begin());
-    }
-  
-  while(forbidden_end.end() != std::find(forbidden_end.begin(), forbidden_end.end(), str.back()))
-    {
-      str.pop_back();
-    }
-  
-  
-  if(str[str.size() - 2] == '\'' && str.back() == 's')
-    {
-      str.erase(str.size() - 2);
-    }
-  
-  std::transform(str.begin(), str.end(), str.begin(), ::tolower);
-}
+      if(str[str.size() - 2] == '\'' && str.back() == 's')
+	{
+	  str.erase(str.size() - 2);
+	}
+      return str;
+    };
 
-bool worder::islegal(std::string str)
-{
-  bool validator{str.length() > 2 && str.back() != '-' && str.front() != '-' &&str.find("--") == std::string::npos};
-  
-  auto lol = [&validator](char c)
+  auto legalize = [](std::string str)
     {
-      if(!isalpha(c) && c != '-')
-	{
-	  validator = false;
-	}
-    };  
-  std::for_each(str.begin(), str.end(), lol);
-    return validator;
+      bool validator{str.length() > 2 && str.back() != '-' && str.front() != '-' &&str.find("--") == std::string::npos};
+  
+      auto lol = [&validator](char c)
+      {
+	if(!isalpha(c) && c != '-')
+	  {
+	    validator = false;
+	  }
+      };  
+      std::for_each(str.begin(), str.end(), lol);
+      return validator;
+    }; 
+  std::vector<std::string> strings;
+  std::vector<std::string> strings2;
+  copy(std::istream_iterator<std::string>(is),std::istream_iterator<std::string>(),inserter(strings,strings.begin()));
+  transform(strings.begin(),strings.end(),strings.begin(),cleanse);
+  copy_if(strings.begin(),strings.end(),inserter(strings2,strings2.begin()),legalize);
+  return strings2;
 }
- 
--- tester.cc
+++ tester.cc
@@ -2,17 +2,23 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include<sstream>
 using namespace std;
-int main(int argc ,char* argv[])
-{
-  if(argc > 1)
-    {
-      ifstream is{argv[1]};
-      worder word_list{is};
-      cout<<endl;
-      cout<<endl;
-      word_list.out_data(cout);
-    }
 
+int main(int argc, char *argv[]) {
+  if(argc>2)
+    {
+      ifstream is(argv[1],ifstream::in);
+      worder word_list{is};
+      unsigned int t{};
+      string s = argv[2];
+      if(argc > 3)
+	{
+	   t = stoi(argv[3]);
+	}
+      word_list.out_data(cout,s[1],t);
+      is.close();
+      cout<<'\n';
+    }
   return 0;
 }
